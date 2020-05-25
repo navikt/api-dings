0edec02151f562a28a4c2aad041527ff6151f193
yousher <youssef.bel.mekki@nav.no>
2020-05-25 13:37:22 +0200

    Aktiverer authentication

--- src/main/kotlin/no/nav/dingsvalidate/Dingsvalidate.kt
+++ src/main/kotlin/no/nav/dingsvalidate/Dingsvalidate.kt
@@ -3,12 +3,15 @@ package no.nav.dingsvalidate
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.authenticate
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever
+import no.nav.security.token.support.ktor.tokenValidationSupport
 import no.nav.security.token.support.test.FileResourceRetriever
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -22,18 +25,18 @@ fun Application.module(enableMock: Boolean = this.environment.config.property("n
     val config = this.environment.config
     val applicationStatus = ApplicationStatus()
 
-    // install(Authentication) {
-    //     if (enableMock)
-    //         tokenValidationSupport(config = config, resourceRetriever = mockResourceRetriever)
-    //     else
-    //         tokenValidationSupport(config = config)
-    // }
+    install(Authentication) {
+        if (enableMock)
+            tokenValidationSupport(config = config, resourceRetriever = mockResourceRetriever)
+        else
+            tokenValidationSupport(config = config)
+    }
 
     install(Routing) {
-        // authenticate {
-        get("/hello") {
-            call.respondText("<b>Authenticated hello</b>", ContentType.Text.Html)
-            //   }
+        authenticate {
+            get("/hello") {
+                call.respondText("<b>Authenticated hello</b>", ContentType.Text.Html)
+            }
         }
 
         get("/openhello") {
